4ae2583ea4cf5c375cfccc9433bd88ca30b3d041
null0verflow <bobdinh139@gmail.com>
2021-04-20 19:33:44 -0400

    message to group

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -37,11 +37,43 @@ public class ChatServer {
  
     void broadcast(String message, UserThread excludeUser) {
         boolean privateConverse = false;
+        boolean groupconversation = false;
+        int count = 0;
+        for (int i = 0; i < message.length(); i++) {
+            if (message.charAt(i) == '@') {
+                count++;
+            }
+        }
+        if (count == 1) privateConverse = true;
+        else if (count > 1) groupconversation = true;
         String toUser = "";
         String meee = message;
         String whosent = "";
         meee = meee.substring(meee.indexOf(":")+2);
         whosent = message.substring(0, message.indexOf(":"));
+       
+        if (groupconversation) {   	
+        	String[] splited = meee.split("\\s+");
+        	String c = "";
+        	String[] userGroup = new String[count];
+        	for (int ii = 0; ii < splited.length; ii++) {
+        		if (splited[ii].indexOf("@") != -1)  {
+        			 userGroup[ii] =  splited[ii].replace("@", "");
+        		} else {
+        		   c += splited[ii] + " ";
+        		}
+        	}
+        	 for (UserThread user : userThreads) {
+              for (int ii = 0; ii < userGroup.length; ii++)
+        		 if (user.getUsername().equals(userGroup[ii])) {
+        			 user.sendMessage(whosent + ": "+c);
+                 }
+                 
+               }
+        	
+        }
+        
+        if (privateConverse) {       
         if (meee.substring(0,1).equals("@")) {
         	 privateConverse = true;
         	 toUser = meee.replace("@", "");
@@ -55,10 +87,11 @@ public class ChatServer {
                       user.sendMessage(whosent + ": "+c);
                   }
               }
+         }
         }
-        
-        
-        if (!privateConverse)
+               
+        //broadcast to the entire server and whoever connected to the server.
+        if (!privateConverse && !groupconversation)
         for (UserThread user : userThreads) {
             if (user != excludeUser) {
                 user.sendMessage(message);
