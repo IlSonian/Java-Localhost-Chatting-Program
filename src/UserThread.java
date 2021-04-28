39c24277d0052f7589f482907b92e92b751caa60
null0verflow <bobdinh139@gmail.com>
2021-04-28 15:14:32 -0400

    clean up + comment my code

--- src/UserThread.java
+++ src/UserThread.java
@@ -1,180 +1,149 @@
 import java.io.*;
 import java.net.*;
- 
+
 
 public class UserThread extends Thread {
-    private Socket socket;
-    private ChatServer server;
-    private PrintWriter writer;
-    String userName;
-    String userNameAndPassword;
-    String password;
-    String up;
-    public UserThread(Socket socket, ChatServer server) {
-        this.socket = socket;
-        this.server = server;
-    }
-    
-    public void setUsername(String name) {
-    	userName = name;
-    }
-    
-    public String getUsername() {
-    	return userName;
-    }
-    
-    public void login(BufferedReader reader) throws IOException {
-    	 
-    	while (up.equals("in") && !server.checkUserNameAndPassword(userName, password)) {
-		userNameAndPassword = reader.readLine();
-        String[] splited = userNameAndPassword.split("\\s+");
-        
-        up = splited[0];
-        userName = splited[1];
-        password = splited[2];
-        server.wrongPass("Wrong pass", this);
-         
-        }  
-        	
-        if (up.equals("up")) {
-        	signup(reader);
-        } else {
-        	server.correctPass("Correct pass", this);   
-            String serverMessage = "#" + server.getUserNames();
-            server.giveListOfUsers(serverMessage, this);
-        }
-    }
-    
-    public void signup (BufferedReader reader) throws IOException {
-        	System.out.println("sign up");
-        	while(server.checkUserNameDuplication(userName) && up.equals("up")) { 
-				  userNameAndPassword = reader.readLine();
-                  String[] splited = userNameAndPassword.split("\\s+");                 
-                  up = splited[0];
-                  userName = splited[1];
-                  password = splited[2];
-                  System.out.println("Duuplicates");
-                  server.wrongPass("Duplicate username", this);
-            }
-        	if (up.equals("in")) {
-        		login(reader);
-        	} else {
-            server.correctPass("Good", this);   
-           	server.signUp(userName, password);
-            String serverMessage = "#" + server.getUserNames();
-            server.giveListOfUsers(serverMessage, this);
-        	}
-        
-    }
-    
-    public void run() {
-        try {
-            InputStream input = socket.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
- 
-            OutputStream output = socket.getOutputStream();
-            writer = new PrintWriter(output, true);
- 
-            userNameAndPassword = reader.readLine();
-            String[] splited = userNameAndPassword.split("\\s+");
-            
-            up = splited[0];
-            userName = splited[1];
-            password = splited[2];
-            
-            if (up.equals("in")) {
-            	login(reader);
-            } else if (up.equals("up")) {
-            	signup(reader);
-            }
-            /*
-            do {
-            userNameAndPassword = reader.readLine();
-            String[] splited = userNameAndPassword.split("\\s+");
-            
-            up = splited[0];
-            userName = splited[1];
-            password = splited[2];
-             if (up.equals("in") && !server.checkUserNameAndPassword(userName, password))
-             server.wrongPass("Wrong pass", this);
-             
-            } while (up.equals("in") && !server.checkUserNameAndPassword(userName, password)); 
-            	
-            if (up.equals("in"))
-            if(server.checkUserNameAndPassword(userName, password)) {
-            	server.correctPass("Correct pass", this);   
-                String serverMessage = "#" + server.getUserNames();
-                server.giveListOfUsers(serverMessage, this);
-             }
-             
-             */
-            
-            /*
-            if(up.equals("up") ) {
-            	System.out.println("sign up");
-            	while(server.checkUserNameDuplication(userName)) { 
-            		  userNameAndPassword = reader.readLine();
-                      String[] splited = userNameAndPassword.split("\\s+");                 
-                      up = splited[0];
-                      userName = splited[1];
-                      password = splited[2];
-                      System.out.println("Duuplicates");
-                      server.wrongPass("Duplicate username", this);
-                }
-               	server.signUp(userName, password);
-                String serverMessage = "#" + server.getUserNames();
-                server.giveListOfUsers(serverMessage, this);
-            }
-            */
-            
-            server.addUserName(userName);
-            String serverMessage = "#" + server.getUserNames();
-            server.giveListOfUsers2(serverMessage, this);
+	private Socket socket;
+	private ChatServer server;
+	private PrintWriter writer;
+	String userName;
+	String userNameAndPassword;
+	String password;
+	String up;
+	public UserThread(Socket socket, ChatServer server) {
+		this.socket = socket;
+		this.server = server;
+	}
 
-      
-            String clientMessage;
- 
-            do {
-                clientMessage = reader.readLine();
-                
-                if (clientMessage == null)
-                	break;
-                else {
-                if (clientMessage.substring(0,2).equals("!#")) {
-                	String oldName = userName;
-                	setUsername(clientMessage.substring(2));
-                	server.changeUserName(oldName, clientMessage.substring(2), this);
-                }
-                else if (clientMessage.substring(0,2).equals("$#")) {
-                	server.changePassword(clientMessage.substring(2), userName);
-                } else if (clientMessage.substring(0,2).equals("##")) {
-                	
-                	if(clientMessage.substring(2,3).equals("[")) {
-                	   server.loadUserData2(userName, clientMessage.substring(2), this);
-                	   System.out.println("Gotta do");
-                	} else               	
-                	  server.loadUserData(userName, clientMessage.substring(2), this);
-                } else if (clientMessage.substring(0,2).equals("!!")) {
-                	server.deleteAccount(userName);
-                }
-                else {
-                  serverMessage = "[" + userName + "]: " + clientMessage;
-                  server.broadcast(serverMessage, this);
-                }
-                }
-                
-               
-            } while (true);
-             
-        } catch (IOException ex) {
-        	this.stop();
-        	ex.printStackTrace();
-        	
-        }
-    }
-    
-    
-    void sendMessage(String message) {
-        writer.println(message);
-    }
+	public void setUsername(String name) {
+		userName = name;
+	}
+
+	public String getUsername() {
+		return userName;
+	}
+   // login and check credentials
+	public void login(BufferedReader reader) throws IOException {
+
+		while (up.equals("in") && !server.checkUserNameAndPassword(userName, password)) {
+			userNameAndPassword = reader.readLine();
+			String[] splited = userNameAndPassword.split("\\s+");
+
+			up = splited[0];
+			userName = splited[1];
+			password = splited[2];
+			server.wrongPass("Wrong pass", this);
+
+		}  
+
+		if (up.equals("up")) {
+			signup(reader);
+		} else {
+			server.correctPass("Correct pass", this);   
+			String serverMessage = "#" + server.getUserNames();
+			server.giveListOfUsers(serverMessage, this);
+		}
+	}
+
+	//sing up 
+	public void signup (BufferedReader reader) throws IOException {
+		System.out.println("sign up");
+		while(server.checkUserNameDuplication(userName) && up.equals("up")) { 
+			userNameAndPassword = reader.readLine();
+			String[] splited = userNameAndPassword.split("\\s+");                 
+			up = splited[0];
+			userName = splited[1];
+			password = splited[2];
+			System.out.println("Duuplicates");
+			server.wrongPass("Duplicate username", this);
+		}
+		if (up.equals("in")) {
+			login(reader);
+		} else {
+			server.correctPass("Good", this);   
+			server.signUp(userName, password);
+			String serverMessage = "#" + server.getUserNames();
+			server.giveListOfUsers(serverMessage, this);
+		}
+
+	}
+
+	@SuppressWarnings("deprecation")
+	public void run() {
+		try {
+			InputStream input = socket.getInputStream();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+			OutputStream output = socket.getOutputStream();
+			writer = new PrintWriter(output, true);
+
+			userNameAndPassword = reader.readLine();
+			String[] splited = userNameAndPassword.split("\\s+");
+
+			up = splited[0];
+			userName = splited[1];
+			password = splited[2];
+
+			// once check for sign up and sign in procceed to add username to the list
+			if (up.equals("in")) {
+				login(reader);
+			} else if (up.equals("up")) {
+				signup(reader);
+			}
+
+			server.addUserName(userName);
+			String serverMessage = "#" + server.getUserNames();
+			server.giveListOfUsers(serverMessage, this);
+
+
+			String clientMessage;
+
+			// listen to the client
+			do {
+				clientMessage = reader.readLine();
+
+				if (clientMessage == null)
+					break;
+				else {
+					// if client request changing username
+					if (clientMessage.substring(0,2).equals("!#")) {
+						String oldName = userName;
+						setUsername(clientMessage.substring(2));
+						server.changeUserName(oldName, clientMessage.substring(2), this);
+					}
+					// if client request changing password
+					else if (clientMessage.substring(0,2).equals("$#")) {
+						server.changePassword(clientMessage.substring(2), userName);
+					} else if (clientMessage.substring(0,2).equals("##")) {
+
+						if(clientMessage.substring(2,3).equals("[")) {
+							server.loadUserData2(userName, clientMessage.substring(2), this);
+							System.out.println("Gotta do");
+						} else               	
+							server.loadUserData(userName, clientMessage.substring(2), this);
+						// if user request delete an account
+					} else if (clientMessage.substring(0,2).equals("!!")) {
+						server.deleteAccount(userName);
+					}
+					else {
+						// if client does not request anything, just send nad broadcast message
+						serverMessage = "[" + userName + "]: " + clientMessage;
+						server.broadcast(serverMessage, this);
+					}
+				}
+
+
+			} while (true);
+
+		} catch (IOException ex) {
+			this.stop();
+			ex.printStackTrace();
+
+		}
+	}
+
+    // send message to client
+	void sendMessage(String message) {
+		writer.println(message);
+	}
 }
--- src/Users.java
+++ src/Users.java
@@ -3,9 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 public class Users {
-      
-      private ArrayList<Group> group = new ArrayList<Group>();
-	 
+      	 
       Group grouppy;
       
 	  private String username;
@@ -43,29 +41,5 @@ public class Users {
 	  public String getPsswd() {
 		  return psswd;
 	  }
-	  
-	  public void writeDataTofile() {	  
-// sombody try to finish writting data to file please
-		      try (PrintWriter writer = new PrintWriter(new File("data.csv"))) {
-
-		        StringBuilder sb = new StringBuilder();
-		        String headerForStuff = "username, password";
-		        sb.append(headerForStuff);
-		        sb.append('\n');
-		        sb.append("bob2, 2215249148");
-		        sb.append('\n');
-
-		        writer.write(sb.toString());
-
-		        System.out.println("done!");
-
-		      } catch (FileNotFoundException e) {
-		        System.out.println(e.getMessage());
-		      }
-
-	
-	
-	  }
-
-      
+	        
 }
