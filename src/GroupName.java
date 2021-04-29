ff1ea6b3717f49cfcdb73a3eb62082df4138f9ec
cassidy7 <77703213+cassidy7@users.noreply.github.com>
2021-04-29 16:04:31 -0400

    Update GroupName.java
    Updates including coding standards

--- src/GroupName.java
+++ src/GroupName.java
@@ -23,88 +23,98 @@ import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.AbstractListModel;
 
+/**
+ * GroupName.java
+ * <p>
+ * group name class
+ * <p>
+ * a list of your sources of help (if any)
+ *
+ * @author Project 5 group
+ * @version 4/29/2021
+ */
 public class GroupName extends JFrame {
 
-	private JTextField messagetext;
-	private JPanel contentPane;
+    private JTextField messagetext;
+    private JPanel contentPane;
 
-	createGroup createG = new createGroup();
+    createGroup createG = new createGroup();
 
-	public GroupName() {
+    public GroupName() {
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		setBounds(100, 100, 743, 446);
+        setBounds(100, 100, 743, 446);
 
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		setLocationRelativeTo(null);
-		contentPane.setLayout(null);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        setLocationRelativeTo(null);
+        contentPane.setLayout(null);
 
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Chat", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
+        JPanel panel = new JPanel();
+        panel.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Chat", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 
-		panel.setBounds(10, 24, 693, 324);
+        panel.setBounds(10, 24, 693, 324);
 
-		contentPane.add(panel);
-		panel.setLayout(null);
+        contentPane.add(panel);
+        panel.setLayout(null);
 
-		JLabel nameTheGroup = new JLabel("Name the group");
-		nameTheGroup.setFont(new Font("Tahoma", Font.BOLD, 14));
+        JLabel nameTheGroup = new JLabel("Name the group");
+        nameTheGroup.setFont(new Font("Tahoma", Font.BOLD, 14));
 
-		nameTheGroup.setBounds(322, -55, 200, 200);
+        nameTheGroup.setBounds(322, -55, 200, 200);
 
-		panel.add(nameTheGroup);
+        panel.add(nameTheGroup);
 
-		JLabel usersSelected = new JLabel("Participants: " + createG.selectedGroupUsers(createG.users));
-		System.out.println(" The selected blah blah is: " + createG.selectedGroupUsers(createG.users)); // test out to see what's the output
-		usersSelected.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		usersSelected.setBounds(100, 50, 200, 200);
-		panel.add(usersSelected);
+        JLabel usersSelected = new JLabel("Participants: " + createG.selectedGroupUsers(createG.users));
+        System.out.println(" The selected blah blah is: " + createG.selectedGroupUsers(createG.users)); // test out to see what's the output
+        usersSelected.setFont(new Font("Tahoma", Font.PLAIN, 14));
+        usersSelected.setBounds(100, 50, 200, 200);
+        panel.add(usersSelected);
 
-		messagetext = new JTextField();
+        messagetext = new JTextField();
 
-		messagetext.setBounds(61, 269, 149, 28);
+        messagetext.setBounds(61, 269, 149, 28);
 
-		panel.add(messagetext);
+        panel.add(messagetext);
 
-		messagetext.setColumns(2);
+        messagetext.setColumns(2);
 
-		JButton createBtn = new JButton("Create");
-		createBtn.setBounds(217, 269, 112, 29);
-		panel.add(createBtn);
+        JButton createBtn = new JButton("Create");
+        createBtn.setBounds(217, 269, 112, 29);
+        panel.add(createBtn);
 
-		JButton backButton = new JButton("Back");
-		backButton.setBounds(571, 358, 119, 31);
-		contentPane.add(backButton);
+        JButton backButton = new JButton("Back");
+        backButton.setBounds(571, 358, 119, 31);
+        contentPane.add(backButton);
 
-		createBtn.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+        createBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
 
-				//TODO: take the input of the user for group name
-				if (e.getSource() == createBtn) {
-					Messagelist msgList = new Messagelist();
-					msgList.setVisible(true);
+                //TODO: take the input of the user for group name
+                if (e.getSource() == createBtn) {
+                    Messagelist msgList = new Messagelist();
+                    msgList.setVisible(true);
 
-					dispose();
-				}
-			}
+                    dispose();
+                }
+            }
 
-		});
+        });
 
-		//simply returns to the previous page
-		backButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				//creating object of Messagelist class
-				createGroup obj = new createGroup(Login.array);
-				obj.setVisible(true);
-				//close current gui
-				dispose();
+        //simply returns to the previous page
+        backButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //creating object of Messagelist class
+                createGroup obj = new createGroup(Login.array);
+                obj.setVisible(true);
+                //close current gui
+                dispose();
 
-			}
-		});
+            }
+        });
 
-	}
+    }
 
 }
