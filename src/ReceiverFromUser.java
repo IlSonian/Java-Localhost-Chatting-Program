73b3916a700e7d01bc7a1c2ca72a1728a42db67f
cassidy7 <77703213+cassidy7@users.noreply.github.com>
2021-04-29 16:09:19 -0400

    Update ReceiverFromUser.java
    Updates including coding standards

--- src/ReceiverFromUser.java
+++ src/ReceiverFromUser.java
@@ -1,41 +1,49 @@
 import java.net.Socket;
 import java.util.ArrayList;
 
+/**
+ * ReceiverFromUser.java
+ * <p>
+ * receiver from user class
+ * <p>
+ * a list of your sources of help (if any)
+ *
+ * @author Project 5 group
+ * @version 4/29/2021
+ */
 public class ReceiverFromUser {
 
+    static String[] array;
+
+    static Socket socket;
+    static boolean in = false;
+    static String myUsername = "";
+    static String mypassword = "";
+    static ArrayList<String> removedList = new ArrayList<>();
+
+    static public void assingSocket(Socket socket) {
+        ReceiverFromUser.socket = socket;
+    }
 
 
-	static String[] array; 
+    static public Socket getSocket() {
+        return socket;
+    }
 
+    static String[] getAllUsers() {
 
-	static Socket socket;
-	static boolean in = false;
-	static String myUsername = "";
-	static String mypassword = "";
-	static ArrayList<String> removedList = new ArrayList<>();
+        return array;
+    }
 
-	static public void assingSocket(Socket socket) {
-		ReceiverFromUser.socket = socket;
-	}
+    static void importUserfromServer(String totalUser) {
+        // adding dummy data to list
+        totalUser = totalUser.replace("[", "");
+        totalUser = totalUser.replace("]", "");
+        System.out.println(totalUser);
+        array = totalUser.split(",");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = array[i].trim();
+        }
 
-
-	static public Socket getSocket() {
-		return socket;
-	}
-	static String[] getAllUsers() {
-
-		return array;
-	}
-
-	static void importUserfromServer(String totalUser) {
-		// adding dummy data to list		    
-		totalUser = totalUser.replace("[", "");
-		totalUser = totalUser.replace("]", "");
-		System.out.println(totalUser);
-		array = totalUser.split(",");
-		for (int i = 0; i < array.length; i++) {
-			array[i] =  array[i].trim();
-		}
-
-	}
+    }
 }
