9d32073bf228cfe822db5896f61c0d252f8c34c9
cassidy7 <77703213+cassidy7@users.noreply.github.com>
2021-04-29 15:57:23 -0400

    Update Group.java
    Updates including coding standards

--- src/Group.java
+++ src/Group.java
@@ -1,37 +1,51 @@
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 
+/**
+ * Group.java
+ * <p>
+ * group class
+ * <p>
+ * a list of your sources of help (if any)
+ *
+ * @author Project 5 group
+ * @version 4/29/2021
+ */
 public class Group {
-   private String groupName;
-   ArrayList<String> userList;
-   public Group(String groupName, ArrayList<String> userList ) {
-	     this.groupName = groupName;
-	     this.userList = userList;
-   }
+    private String groupName;
+    ArrayList<String> userList;
 
-   public String getGroupName() {
-	    return groupName;
-   }
-   public ArrayList<String> getUserList() {
-	   return userList;
-   }
-   public void setGroupName(String groupName) {
-	   this.groupName = groupName;
-   }
-   public void setGroupMember(ArrayList<String> userList) {
-	   LinkedHashSet<String> hashSet = new LinkedHashSet<>(userList);
-       
-       ArrayList<String> listWithoutDuplicates = new ArrayList<>(hashSet);
-	   this.userList = listWithoutDuplicates;
-   }
-   
-   public String toString() {
-	   String c = "";
-	   for (int i = 0 ; i< userList.size(); i++) {
-		   c += userList.get(i) + "; ";
-	   }
-	   c = c.substring(0, c.length() - 2);
-	   return c;
-   }
- 
+    public Group(String groupName, ArrayList<String> userList) {
+        this.groupName = groupName;
+        this.userList = userList;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public ArrayList<String> getUserList() {
+        return userList;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public void setGroupMember(ArrayList<String> userList) {
+        LinkedHashSet<String> hashSet = new LinkedHashSet<>(userList);
+
+        ArrayList<String> listWithoutDuplicates = new ArrayList<>(hashSet);
+        this.userList = listWithoutDuplicates;
+    }
+
+    public String toString() {
+        String c = "";
+        for (int i = 0; i < userList.size(); i++) {
+            c += userList.get(i) + "; ";
+        }
+        c = c.substring(0, c.length() - 2);
+        return c;
+    }
+
 }
